6a00c1ece9f382e44af1a6980cd52952a5015593
Szymulcio <szymonwojnarowski@outlook.com>
2021-09-14 03:51:15 +0200

    Added dropdown menu

--- main.js
+++ main.js
@@ -13,3 +13,18 @@ toggleButton.addEventListener("click", () => {
     menuOpen = false;
   }
 });
+
+var acc = document.getElementsByClassName("dropdown-menu");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
